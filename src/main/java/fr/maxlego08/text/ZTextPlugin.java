f64d428bd3b023b7601c58f6dccb837c9e2634a0
Maxlego08 <maxlego08@sfr.fr>
2025-03-20 22:22:30 +0100

    :tada: First commit !

---
+++ src/main/java/fr/maxlego08/text/ZTextPlugin.java
@@ -0,0 +1,17 @@
+package fr.maxlego08.text;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZTextPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
