ca3256427b04e73f672cb4486280f1abdfe191a9
Maxlego08 <maxlego08@sfr.fr>
2025-11-02 16:58:04 +0100

    :memo: try fix action

--- Hooks/ItemsAdder/build.gradle.kts
+++ Hooks/ItemsAdder/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 dependencies {
 
     compileOnly(projects.api)
-
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("dev.lone:api-itemsadder:4.0.2-beta-release-11")
     compileOnly(files("libs/LoneLibs_1.0.65.jar"))
 }
